ba9a67b7946cd594e7c216bab5ddc5f10805648d
Pablo Espinosa <espipj@gmail.com>
2023-01-29 16:07:20 +0000

    scraping several pages

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 require("dotenv").config();
-import { Telegraf } from "telegraf";
+import { Context, NarrowedContext, Telegraf } from "telegraf";
 import { load } from "cheerio";
 import axios from "axios";
-import { Property } from "./types";
+import { Property, SearchResult } from "./types";
+import { Update, Message } from "telegraf/typings/core/types/typegram";
 
 const bot = new Telegraf(process.env.TG_BOT_TKN || "");
 const chatId = process.env.TG_CHAT || "";
@@ -18,9 +19,31 @@ const getAdData = (id: string | number) => {
       const propertyData: Property = JSON.parse(
         scriptData.slice(25, scriptData.length)
       );
-      console.log("response");
       return propertyData;
-    });
+    })
+    .catch((e) => console.log(e));
+};
+
+const scrapPropertyData = async (code: number | string) => {
+  const propertyData = await getAdData(code);
+  if (!propertyData) return null;
+  const textMessage = `${propertyData.propertyData.address.displayAddress} \n${propertyData.propertyData.prices.primaryPrice}`;
+  const imagesArray = propertyData.propertyData.images.map((v) => ({
+    media: v.url,
+    caption: v.caption,
+    type: "photo",
+  }));
+  imagesArray.push({
+    media: propertyData.propertyData.staticMapImgUrls.staticMapImgUrlMobile,
+    caption: "Area",
+    type: "photo",
+  });
+  const location = {
+    latitude: propertyData.propertyData.location.latitude,
+    longitude: propertyData.propertyData.location.longitude,
+  };
+
+  return { textMessage, location, imagesArray };
 };
 
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
@@ -63,4 +86,59 @@ bot.command(["property", "pp"], async (ctx) => {
   );
 });
 
-bot.launch();
+bot.command(["ll", "lastlistings"], async (ctx) => {
+  ctx.reply("Hello");
+  scrap(ctx);
+});
+
+type ctxType = NarrowedContext<
+  Context<Update>,
+  {
+    message: Update.New & Update.NonChannel & Message.TextMessage;
+    update_id: number;
+  }
+>;
+const scrap = async (ctx: ctxType) => {
+  let pages = 1;
+  for (let p = 0; p < pages; p++) {
+    const srchURL = `https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=USERDEFINEDAREA%5E%7B%22id%22%3A%226270618%22%7D&minBedrooms=2&maxPrice=350000&index=${
+      p * 24
+    }`;
+    console.log(srchURL);
+
+    try {
+      const response = await axios.get(srchURL);
+
+      const html = response.data;
+      const $ = load(html);
+
+      console.log(response.status);
+      const scriptData = $("body > script:nth-child(11)").text();
+      const propertyData: SearchResult = JSON.parse(
+        scriptData.slice(19, scriptData.length)
+      );
+      pages = propertyData.pagination.total;
+
+      for (let i = 0; i < propertyData.properties.length; i++) {
+        const ppty = await scrapPropertyData(propertyData.properties[i].id);
+        if (!ppty) continue;
+        console.log(ppty.textMessage);
+        await ctx.sendMessage(ppty?.textMessage);
+        await ctx.sendMediaGroup(
+          /* @ts-ignore */
+          ppty?.imagesArray.slice(0, 10)
+        );
+        await ctx.sendLocation(
+          ppty?.location.latitude,
+          ppty.location.longitude
+        );
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+};
+
+scrap({} as any);
+
+// bot.launch();
